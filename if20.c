e6a28af5b70f9d6a1a2b61d3620347b04b2795cb
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:35:16 +0530

    Add files via upload
    Solved Smallest Using Ternary Operator in C programming

---
+++ if20.c
@@ -0,0 +1,13 @@
+//20. Smallest Using Ternary Operator
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int smallest = (a < b) ? a : b;
+    printf("%d is smallest", smallest);
+
+    return 0;
+}
