26e7553fa33cd23722b28b214e4cbdf2d3324608
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:34:19 +0530

    Add files via upload
    Solved Electricity Bill in C programming

---
+++ if18.c
@@ -0,0 +1,20 @@
+//18. Electricity Bill (Using If-Else)
+
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+    scanf("%d", &units);
+
+    if(units <= 50){
+        bill = units * 3;
+    }else if (units <= 100){
+        bill = (50 * 3) + (units - 50) * 4;
+    }else{
+        bill = (50 * 3) + (50 * 4) + (units - 100) * 5;
+    }
+    printf("Bill amount = Rs. %.0f", bill);
+    
+    return 0;
+}
