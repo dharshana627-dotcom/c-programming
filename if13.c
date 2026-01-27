b9c939b4736a530711fcdf0ba51f16e6ae90452e
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:31:29 +0530

    Add files via upload
    Solved Uppercase or Lowercase in C programming

---
+++ if13.c
@@ -0,0 +1,17 @@
+//13. Uppercase or Lowercase Letter
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z'){
+        printf("Uppercase letter");
+    }else if(ch >= 'a' && ch <= 'z'){
+        printf("Lowercase letter");
+    }else{
+        printf("Not an alphabet");
+    }
+    return 0;
+}
