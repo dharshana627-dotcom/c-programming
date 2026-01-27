995bd08d789d2207fde879407e5b845de05bca08
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:29:17 +0530

    Add files via upload
    Solved Voting Eligibility in C programming

---
+++ if9.c
@@ -0,0 +1,15 @@
+//9.Voting Eligibility
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if(a >= 18){
+        printf("Eligible to vote");
+    }else{
+        printf("Not Eligible to vote");
+    }
+    return 0;
+}
