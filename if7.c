c291e812a704f60299ecca5aca022de708c735f9
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:27:47 +0530

    Add files via upload
    Solved Alphabet, Digit, or Special Character in C programming

---
+++ if7.c
@@ -0,0 +1,17 @@
+//7. Alphabet, Digit, or Special Character
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z'){
+        printf("Alphabet");
+    }else if(ch >= '0' && ch <= '9'){
+        printf("Digit");
+    }else{
+        printf("Special character");
+    }
+    return 0;
+}
