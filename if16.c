6c432c6ebd6f858e564bb24e6cff31aed8cea57f
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:33:24 +0530

    Add files via upload
    Solved Type of Triangle in C programming

---
+++ if16.c
@@ -0,0 +1,17 @@
+//16. Type of Triangle (Using Sides)
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c){
+        printf("Equilateral triangle");
+    }else if (a == b || b == c || a == c){
+        printf("Isosceles triangle");
+    }else{
+        printf("Scalene triangle");
+    }
+    return 0;
+}
