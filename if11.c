e5062e862ce941fb49059a92b2f0c10d7e2dd43e
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-27 21:30:27 +0530

    Add files via upload
    Solved Largest of Three Numbers in C programming

---
+++ if11.c
@@ -0,0 +1,19 @@
+//11.Largest of three numbers
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a >= b && a >= c) {
+        printf("%d is largest", a);
+    }
+    else if (b >= a && b >= c) {
+        printf("%d is largest", b);
+    }
+    else {
+        printf("%d is largest", c);
+    }
+
+    return 0;
+}
