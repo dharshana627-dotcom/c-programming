14c59f2ed3ad5d5a682f1e6710ac44ee77c59fb4
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-28 16:04:47 +0530

    Add files via upload
    Solved Even Numbers Printer in C programming

---
+++ loop3.c
@@ -0,0 +1,14 @@
+//3.Even Numbers Printer
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i = 1;i <= n;i++) {
+        if (i % 2 == 0) {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
