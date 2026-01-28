f7706f3cb9904ca313fb12854259bf12b1953b7e
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-28 16:10:01 +0530

    Add files via upload
    Solved Sum of Even Numbers in C programming

---
+++ loop7.c
@@ -0,0 +1,16 @@
+//7.Sum of Even Numbers
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum = 0;
+    scanf("%d",&n);
+    for (int i = 1;i <= n;i++) {
+        if (i % 2 == 0) {
+            sum = sum + i;
+        }
+        }
+    printf("%d",sum);
+    return 0;
+}
