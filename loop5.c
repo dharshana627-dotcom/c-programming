9c6680be5ac40673d85a4b85e737153547e5d28b
Dharshana Ganesan <dharshana627@gmail.com>
2026-01-28 16:06:33 +0530

    Add files via upload
    Solved Countdown Timer in C programming

---
+++ loop5.c
@@ -0,0 +1,13 @@
+//5.Countdown Timer
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i = n;i >= 1;i--) {
+        printf("%d ",i);
+        }
+    return 0;
+}
+
