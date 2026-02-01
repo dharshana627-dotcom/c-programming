435ff8288d22dfbae83f2fa8c896af14623d3789
Dharshana Ganesan <dharshana627@gmail.com>
2026-02-01 18:06:28 +0530

    Add files via upload

---
+++ While_loop/w15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,hours;
+    int sum=0,count=0;
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n){
+        scanf("%d",&hours);
+        sum=sum+hours;
+        if(hours>4){
+            count++;
+        }
+        i++;
+    }
+    printf("Total Overtime: %d\n",sum);
+    printf("Burnout Days: %d",count);
+    return 0;
+}
