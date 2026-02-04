54705c410d4b87460618eae26167b4a2425215ff
Dharshana Ganesan <dharshana627@gmail.com>
2026-02-04 15:45:26 +0530

    Add files via upload

---
+++ 1D Array/arr24.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+    }
+    if (count==0) {
+        printf("Yes\n");
+    }else {
+        printf("No\n");
+    }
+    return 0;
+}
