a9f403aac4476c76650b4e57410ace1581381b73
Dharshana Ganesan <dharshana627@gmail.com>
2026-02-08 19:35:11 +0530

    Add files via upload

---
+++ 2D Arrays/arr16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int r,c;
+    scanf("%d %d", &r, &c);
+    int arr[r][c];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int min = 0,index = -1;
+    for (int i = 0; i < r; i++) {
+        int sum = 0;
+        for (int j = 0; j < c; j++) {
+            sum += arr[i][j];
+        }
+        if (sum > 0) {
+            if (index == -1 || sum < min) {
+                min = sum;
+                index = i;
+            }
+        }
+    }
+    printf("%d", index);
+    return 0;
+}
