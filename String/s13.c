cf8dbcbee96b4bbbf0223d1eedc1966da6acea5c
Dharshana Ganesan <dharshana627@gmail.com>
2026-02-10 10:41:19 +0530

    Add files via upload

---
+++ String/s13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    char ch;
+    int index = -1;
+    scanf("%s %c", str,&ch);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch) {
+            index = i;
+            break;
+        }
+    }
+    printf("%d\n", index);
+    return 0;
+}
